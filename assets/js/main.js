7865838fe43df54ee1c2e6b793a17a27bb2c0877
Christopher Ayoub <43099907+chrock1@users.noreply.github.com>
2018-09-08 20:19:49 -0400

    Delete main.js

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	$(".wrapper").hover(function() {
-        $(this).css("border-color", "gray");
-        $(this).find(".my-link").css("color", "gray");
-    }, function() {
-        $(this).css("border-color", "white");
-        $(this).find(".my-link").css("color", "white");
-    });
-});
